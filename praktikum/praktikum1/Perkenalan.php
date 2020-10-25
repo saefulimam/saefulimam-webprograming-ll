04610a791eaf4b87d1b5847bdde94c4bb478b711
saeful_imam <imamsaeful675@gmail.com>
2020-10-25 14:35:22 +0700

    first commit

---
+++ praktikum/praktikum1/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php 
+class Perkenalan extends CI_Controller{
+	public function index(){
+		echo "<h1>Perkenalkan<h1>";
+		echo "<h2>Nama saya Farkhan Mauriza<br>
+			Saya tinggal didaerah Tegal<br>
+			Olahraga yang saya sukai adalah futsal<h2>";
+	}
+}
+
+
---
+++ pustaka-booking/application/controllers/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class Perkenalan extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan<h1>";
+		echo "<h2>Saeful Imam Gunawan<br>
+			Saya tinggal di Margadana<br>
+			Olahraga yang saya sukai adalah berenang<h2>";
+	}
+}
